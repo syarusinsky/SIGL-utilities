5161d6993edf4a14c4a20f07cf00d53d6a191051
siike92 <siike92@hotmail.com>
2020-06-21 16:54:08 -0500

    Modified font generator to include all characters, added all characters to Smoll font

--- fonts/font-generator.py
+++ fonts/font-generator.py
@@ -25,18 +25,85 @@ else:
 
 font_file = open( font_name + ".sff", 'wb')
 
-lowercase_chars = "abcdefghijklmnopqrstuvwxyz"
+characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 &@\`^:,$=!/>#[{(<-%.|+?\"]});'*~_"
 
 char_index = 0
 bitmap_char_width = 0
 bitmap_width = 0
 bitmap_height = 0
-bitmap_byte_start = 4 # the byte at which the mapping of character to bitmap index begins
+bitmap_byte_start = 7 # the byte at which the mapping of character to bitmap index begins
 
-for char in lowercase_chars:
+for char in characters:
     file_header.append( char )
     file_header.append( char_index )
-    char_img_path = os.path.join( font_dir, char + ".png" )
+    if char == " ":
+        char_img_path = os.path.join( font_dir, "space" + ".png" )
+    elif char == "&":
+        char_img_path = os.path.join( font_dir, "ampersand" + ".png" )
+    elif char == "@":
+        char_img_path = os.path.join( font_dir, "at" + ".png" )
+    elif char == "\\":
+        char_img_path = os.path.join( font_dir, "backslash" + ".png" )
+    elif char == "`":
+        char_img_path = os.path.join( font_dir, "backtick" + ".png" )
+    elif char == "^":
+        char_img_path = os.path.join( font_dir, "carat" + ".png" )
+    elif char == ":":
+        char_img_path = os.path.join( font_dir, "colon" + ".png" )
+    elif char == ",":
+        char_img_path = os.path.join( font_dir, "comma" + ".png" )
+    elif char == "$":
+        char_img_path = os.path.join( font_dir, "dollarsign" + ".png" )
+    elif char == "=":
+        char_img_path = os.path.join( font_dir, "equals" + ".png" )
+    elif char == "!":
+        char_img_path = os.path.join( font_dir, "exclamation" + ".png" )
+    elif char == "/":
+        char_img_path = os.path.join( font_dir, "forwardslash" + ".png" )
+    elif char == ">":
+        char_img_path = os.path.join( font_dir, "greaterthan" + ".png" )
+    elif char == "#":
+        char_img_path = os.path.join( font_dir, "hash" + ".png" )
+    elif char == "[":
+        char_img_path = os.path.join( font_dir, "leftbracket" + ".png" )
+    elif char == "{":
+        char_img_path = os.path.join( font_dir, "leftcurlybracket" + ".png" )
+    elif char == "(":
+        char_img_path = os.path.join( font_dir, "leftparenthesis" + ".png" )
+    elif char == "<":
+        char_img_path = os.path.join( font_dir, "lessthan" + ".png" )
+    elif char == "-":
+        char_img_path = os.path.join( font_dir, "minus" + ".png" )
+    elif char == "%":
+        char_img_path = os.path.join( font_dir, "percent" + ".png" )
+    elif char == ".":
+        char_img_path = os.path.join( font_dir, "period" + ".png" )
+    elif char == "|":
+        char_img_path = os.path.join( font_dir, "pipe" + ".png" )
+    elif char == "+":
+        char_img_path = os.path.join( font_dir, "plus" + ".png" )
+    elif char == "?":
+        char_img_path = os.path.join( font_dir, "questionmark" + ".png" )
+    elif char == "\"":
+        char_img_path = os.path.join( font_dir, "quotes" + ".png" )
+    elif char == "]":
+        char_img_path = os.path.join( font_dir, "rightbracket" + ".png" )
+    elif char == "}":
+        char_img_path = os.path.join( font_dir, "rightcurlybracket" + ".png" )
+    elif char == ")":
+        char_img_path = os.path.join( font_dir, "rightparenthesis" + ".png" )
+    elif char == ";":
+        char_img_path = os.path.join( font_dir, "semicolon" + ".png" )
+    elif char == "'":
+        char_img_path = os.path.join( font_dir, "singlequotes" + ".png" )
+    elif char == "*":
+        char_img_path = os.path.join( font_dir, "star" + ".png" )
+    elif char == "~":
+        char_img_path = os.path.join( font_dir, "tilde" + ".png" )
+    elif char == "_":
+        char_img_path = os.path.join( font_dir, "underscore" + ".png" )
+    else:
+        char_img_path = os.path.join( font_dir, char + ".png" )
     char_img = pygame.image.load( char_img_path )
     bitmap_char_width = char_img.get_width()
     bitmap_width = bitmap_width + char_img.get_width() # individual widths may change in the future
@@ -53,9 +120,15 @@ for char in lowercase_chars:
 
     char_index = char_index + 1
 
-# write character width, width, height, and bitmap byte start
+# get the bytes for bitmap width
+bitmap_width_bytes = bitmap_width.to_bytes( 4, byteorder='big', signed=False )
+
+# write character width (1 byte), width (4 bytes), height (1 byte), and bitmap byte start (1 byte)
 font_file.write( bytes([bitmap_char_width]) )
-font_file.write( bytes([bitmap_width]) )
+font_file.write( bytes([bitmap_width_bytes[0]]) )
+font_file.write( bytes([bitmap_width_bytes[1]]) )
+font_file.write( bytes([bitmap_width_bytes[2]]) )
+font_file.write( bytes([bitmap_width_bytes[3]]) )
 font_file.write( bytes([bitmap_height]) )
 font_file.write( bytes([bitmap_byte_start]) )
 
@@ -77,15 +150,15 @@ for row in range(0, bitmap_height):
 
         byte_accumulator = byte_accumulator + 1
         if byte_accumulator >= 8:
-            for bit in range(0, 8):
-                print( "PIXEL NUM: {} = {}     BYTE NUM: {}".format((byte_num * 8) + bit, (byte_val >> (7 - bit)) & 1, byte_num) )
+            # for bit in range(0, 8):
+                # print( "PIXEL NUM: {} = {}     BYTE NUM: {}".format((byte_num * 8) + bit, (byte_val >> (7 - bit)) & 1, byte_num) )
             font_file.write( bytes([byte_val]) )
             byte_val = 0
             byte_num = byte_num + 1
             byte_accumulator = 0
 
 if byte_accumulator > 0:
-    print( "BYTE: {0} VALUE: {1:#010b}".format(byte_num, byte_val) )
+    # print( "BYTE: {0} VALUE: {1:#010b}".format(byte_num, byte_val) )
     font_file.write( bytes([byte_val]) )
     byte_val = 0
     byte_num = byte_num + 1
