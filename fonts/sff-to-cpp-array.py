ebf034dc22be0b9d613ab8dc40eb04096a82420b
siike92 <siike92@hotmail.com>
2021-03-10 19:05:43 -0600

    Added utility to turn a .sff font file into a .h file with an initialized c array

---
+++ fonts/sff-to-cpp-array.py
@@ -0,0 +1,41 @@
+# A simple script to turn a sff font file into an initialized array of bytes in the c standard
+
+import sys
+import os
+
+this_dir = os.path.dirname( os.path.realpath("__file__") )
+
+if len(sys.argv) == 1:
+    print( "ERROR: you need to specify the name of the sff file to convert" )
+    quit()
+else:
+    font_name = sys.argv[1]
+
+# open files
+try:
+    sff_file = open( font_name + ".sff", 'rb' )
+except IOError:
+    print( "ERROR: font file could not be found" )
+    quit()
+
+cpp_array_file = open( font_name + ".h", 'a' )
+
+# clear h file just in case there's one already there
+cpp_array_file.truncate( 0 )
+
+# create c array
+cpp_array_file.write( "uint8_t " + font_name + "_data[] = { " );
+
+sff_byte = sff_file.read( 1 )
+while sff_byte:
+    byte_string = format( int.from_bytes(sff_byte, "big"), '#010b')
+    sff_byte = sff_file.read( 1 )
+    if sff_byte:
+        byte_string = byte_string + ", "
+    cpp_array_file.write( byte_string )
+
+cpp_array_file.write( " };" );
+
+# close files
+sff_file.close()
+cpp_array_file.close()
